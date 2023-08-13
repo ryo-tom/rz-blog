241d35d662d1169deee7897cef0029b00ea11fa8
rk-techs <ryskdevelop@gmail.com>
2023-08-13 14:03:09 +0900

    feat: add test login routing

--- src/routes/auth.php
+++ src/routes/auth.php
@@ -16,3 +16,22 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
+
+/*
+|--------------------------------------------------------------------------
+| ※※開発用テストログイン※※
+|--------------------------------------------------------------------------
+|
+*/
+Route::middleware('guest')->group(function () {
+    if (app()->environment('local')) {
+        Route::get('login-test/{id}', function ($id) {
+            $user = App\Models\User::find($id);
+            if (!$user) {
+                return "パラメータ: {$user}が間違っています。";
+            }
+            Auth::loginUsingId($id);
+            return redirect('admin');
+        });
+    }
+});
