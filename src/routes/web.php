5a80abc56ee7243c9e2ca7c09e676f07cc408925
rk-techs <ryskdevelop@gmail.com>
2023-08-13 13:57:53 +0900

    feat: login and logout

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin', function () {
+    return view('admin.dashboard');
+});
+
+require __DIR__.'/auth.php';
