33d7f5500441f5054d34e79556d63cf5247ea5d1
rk-techs <ryskdevelop@gmail.com>
2023-08-14 13:17:59 +0900

    define relationships

--- src/app/Models/Post.php
+++ src/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -18,4 +19,18 @@ class Post extends Model
         'is_published',
         'published_at',
     ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Defining Relationships
+    |--------------------------------------------------------------------------
+    */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
