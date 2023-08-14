c2ac90950d5fb9fa4dd46bd3017a47721a9a275f
rk-techs <ryskdevelop@gmail.com>
2023-08-14 10:36:05 +0900

    fix category edit url

--- src/routes/web.php
+++ src/routes/web.php
@@ -30,7 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::get('admin/category', [CategoryController::class, 'index'])->name('admin.category.index');
     Route::get('admin/category/create', [CategoryController::class, 'create'])->name('admin.category.create');
     Route::post('admin/category', [CategoryController::class, 'store'])->name('admin.category.store');
-    Route::get('admin/{category}/edit', [CategoryController::class, 'edit'])->name('admin.category.edit');
+    Route::get('admin/category/{category}/edit', [CategoryController::class, 'edit'])->name('admin.category.edit');
     Route::patch('admin/category/{category}', [CategoryController::class, 'update'])->name('admin.category.update');
     Route::delete('admin/category/{category}', [CategoryController::class, 'destroy'])->name('admin.category.destroy');
 });
