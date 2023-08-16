93486d342ce3c50c97f6ab963b6c2d62c130dfeb
rk-techs <ryskdevelop@gmail.com>
2023-08-17 00:00:15 +0900

    refactor: header layout (#12)
    * remove unnecessary header-block div calss
    
    * style: formatted
    
    * add header height
    
    * fix: move mobile nav element outside of header
    
    Moved the <nav id="mobileNav"> out of <header> to address an issue where fixed header was obscuring underlying content when the nav element was displayed.
    
    * style: rename class and id name about header elements
    
    * fix(css): adjust scroll-padding-top for anchor scrolling offset
    
    * fix(scss): switch from position sticky to fixed for consistent header positioning
    
    changed to address issues with unexpected movements during scrolling browser... (ブラウザの外側？余白部分？が見えるような動き。)

--- src/resources/views/front/_layout/header.blade.php
+++ src/resources/views/front/_layout/header.blade.php
@@ -1,44 +1,42 @@
 <header class="layout-header">
     <div class="header-container">
-        <div class="header-block">
-            {{-- Site Title --}}
-            <div class="site-title-box">
-                <a href="{{ route('home') }}" class="site-title">
-                    <div class="main-title">Rz Note</div>
-                    <div class="sub-title">with <span>Laravel</span></div>
-                </a>
-            </div>
-
-            {{-- Search Form --}}
-            <div id="searchModalTrigger" class="search-box">
-                <span class="material-symbols-outlined">
-                    search
-                </span>
-                <div class="placeholder">
-                    Search
-                </div>
-            </div>
-
-            {{-- Mobile Navigation Button --}}
-            <div id="navBtn" class="navbtn"></div>
-
-            {{-- PC Navigation --}}
-            <nav class="pc-nav">
-                <ul class="nav-list">
-                    <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">HOME</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-                </ul>
-            </nav>
+        {{-- Site Title --}}
+        <div class="site-title-box">
+            <a href="{{ route('home') }}" class="site-title">
+                <div class="main-title">Rz Note</div>
+                <div class="sub-title">with <span>Laravel</span></div>
+            </a>
         </div>
-    </div>
 
-    {{-- Mobile Navigation --}}
-    <nav id="mobileNav" class="mobile-nav collapse">
-        <ul class="nav-list">
-            <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">HOME</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-        </ul>
-    </nav>
+        {{-- Search Form --}}
+        <div id="searchModalTrigger" class="search-box">
+            <span class="material-symbols-outlined">
+                search
+            </span>
+            <div class="placeholder">
+                Search
+            </div>
+        </div>
+
+        {{-- Mobile Navigation Button --}}
+        <div id="mobileNavTrigger" class="mobile-nav-btn"></div>
+
+        {{-- PC Navigation --}}
+        <nav class="pc-nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">HOME</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
+
+{{-- Mobile Navigation --}}
+<nav id="mobileNav" class="mobile-nav collapse">
+    <ul class="nav-list">
+        <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">HOME</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+    </ul>
+</nav>
