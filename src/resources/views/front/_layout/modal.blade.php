53c698724c529a5b1f889d57741eb8e4ece188fe
rk-techs <ryskdevelop@gmail.com>
2023-08-18 07:50:59 +0900

    refactor: extract search element from modal to a separate blade template

--- src/resources/views/front/_layout/modal.blade.php
+++ src/resources/views/front/_layout/modal.blade.php
@@ -1,37 +1,5 @@
 <div id="searchModal" class="modal disable">
     <div class="modal-container">
-      {{-- Search Header --}}
-      <div class="search-header">
-          <form id="search-form" action="" class="search-form">
-              <label for="search-input" class="label-search">
-                  <span class="material-symbols-outlined">
-                      search
-                  </span>
-              </label>
-              <input type="search" id="search-input" placeholder="search" class="search-input">
-          </form>
-          <div id="searchClose" class="search-close">Close</div>
-      </div>
-      {{-- Suggestion Words --}}
-      <div class="suggestion">
-          <span>検索候補:</span>
-          <ul class="suggestion-list">
-              <li class="suggestion-item">laravel</li>
-              <li class="suggestion-item">composer</li>
-              <li class="suggestion-item">install</li>
-          </ul>
-      </div>
-      {{-- Search Mode --}}
-      <div class="search-mode">
-          <span>検索モード:</span>
-      </div>
-      <div class="search-body">
-          検索結果:
-          <ul class="search-results">
-            @for ($i = 0; $i < 30; $i++)
-            <li class="search-result"><a href="">記事タイトル{{$i}}</a></li>
-            @endfor
-          </ul>
-      </div>
+        @include('front._block.search')
     </div>
   </div>
