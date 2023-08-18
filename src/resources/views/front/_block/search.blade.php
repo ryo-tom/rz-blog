b756a0bef656af7345cff36703e976fbf456caf9
rk-techs <ryskdevelop@gmail.com>
2023-08-18 22:49:39 +0900

    update search-block

--- src/resources/views/front/_block/search.blade.php
+++ src/resources/views/front/_block/search.blade.php
@@ -1,28 +1,30 @@
 <div class="search-block">
     {{-- Search Header --}}
     <div class="search-header">
-        <form id="search-form" action="" class="search-form">
-            <label for="search-input" class="label-search">
-                <span class="material-symbols-outlined">
-                    search
-                </span>
-            </label>
-            <input type="search" id="search-input" placeholder="search" class="search-input">
-        </form>
-        <div id="searchClose" class="search-close">Close</div>
-    </div>
-    {{-- Suggestion Words --}}
-    <div class="suggestion">
-        <span>検索候補:</span>
-        <ul class="suggestion-list">
-            <li class="suggestion-item">laravel</li>
-            <li class="suggestion-item">composer</li>
-            <li class="suggestion-item">install</li>
-        </ul>
-    </div>
-    {{-- Search Mode --}}
-    <div class="search-mode">
-        <span>検索モード:</span>
+        <div class="search-bar">
+            <form id="searchForm" action="" class="search-form">
+                <label for="searchInput" class="search-label">
+                    <span class="material-symbols-outlined">
+                        search
+                    </span>
+                </label>
+                <input type="search" id="searchInput" placeholder="Search " class="search-input">
+            </form>
+            <div id="searchClose" class="search-close">Close</div>
+        </div>
+        {{-- Suggestion Words --}}
+        <div class="suggestion-bar">
+            <span>検索候補:</span>
+            <ul class="suggestion-list">
+                <li class="suggestion-item">laravel</li>
+                <li class="suggestion-item">composer</li>
+                <li class="suggestion-item">install</li>
+            </ul>
+        </div>
+        {{-- Search Mode --}}
+        <div class="search-mode">
+            <span>検索モード:</span>
+        </div>
     </div>
     <div class="search-body">
         検索結果:
