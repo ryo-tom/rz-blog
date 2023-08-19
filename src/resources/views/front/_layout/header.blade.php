c0a0c5703b0f02da534f423d78823d6d3e6c2a87
rk-techs <ryskdevelop@gmail.com>
2023-08-19 11:40:39 +0900

    change mobileNav to overlay page

--- src/resources/views/front/_layout/header.blade.php
+++ src/resources/views/front/_layout/header.blade.php
@@ -30,13 +30,13 @@
             </ul>
         </nav>
     </div>
+    {{-- Mobile Navigation --}}
+    <nav id="mobileNav" class="mobile-nav collapse">
+        <ul class="nav-list">
+            <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">HOME</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+        </ul>
+    </nav>
 </header>
 
-{{-- Mobile Navigation --}}
-<nav id="mobileNav" class="mobile-nav collapse">
-    <ul class="nav-list">
-        <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">HOME</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
-    </ul>
-</nav>
