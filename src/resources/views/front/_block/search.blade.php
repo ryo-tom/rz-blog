b55ba12011a3e9448668d2527950a4fe360022b5
rk-techs <ryskdevelop@gmail.com>
2023-08-19 11:02:23 +0900

    refactor(front):  update search-block on modal (#17)
    * refactor: remove unnecessary nesting in Sass styles
    
    * add search-header-height
    
    * add side padding to search-header and remove its child elements
    
    * update search-block
    
    * rename search-close to mobile-modal-close
    
    * rename search-mode to search-scope and update layouts
    
    * style: add comment separators for clearer section demarcation in SCSS and Blade templates
    
    * rename search-body child elements
    
    * update height and padding inside search-block
    
    * add heights variables for calc
    
    * style: update

--- src/resources/views/front/_block/search.blade.php
+++ src/resources/views/front/_block/search.blade.php
@@ -1,34 +1,52 @@
 <div class="search-block">
     {{-- Search Header --}}
     <div class="search-header">
-        <form id="search-form" action="" class="search-form">
-            <label for="search-input" class="label-search">
-                <span class="material-symbols-outlined">
-                    search
-                </span>
-            </label>
-            <input type="search" id="search-input" placeholder="search" class="search-input">
-        </form>
-        <div id="searchClose" class="search-close">Close</div>
+        {{-- Search Bar --}}
+        <div class="search-bar">
+            <form id="searchForm" action="" class="search-form">
+                <label for="searchInput" class="search-label">
+                    <span class="material-symbols-outlined">
+                        search
+                    </span>
+                </label>
+                <input type="search" id="searchInput" placeholder="Search " class="search-input">
+            </form>
+            <div id="mobileModalClose" class="mobile-modal-close">Close</div>
+        </div>
+        {{-- Suggestion Bar --}}
+        <div class="suggestion-bar">
+            <span>検索候補:</span>
+            <ul class="suggestion-list">
+                <li class="suggestion-item">laravel</li>
+                <li class="suggestion-item">composer</li>
+                <li class="suggestion-item">install</li>
+            </ul>
+        </div>
+        {{-- Search Scope Bar --}}
+        <div class="search-scope-bar">
+            <span class="scope-label">検索範囲:</span>
+            <ul class="scope-list">
+                <li class="scope-item">
+                    <input class="scope-radio" type="radio" id="titleMatch" name="searchScope" value="title">
+                    <label class="scope-label-item" for="titleMatch">タイトル</label>
+                </li>
+                <li class="scope-item">
+                    <input class="scope-radio" type="radio" id="contentMatch" name="searchScope" value="content">
+                    <label class="scope-label-item" for="contentMatch">記事内</label>
+                </li>
+                <li class="scope-item">
+                    <input class="scope-radio" type="radio" id="fullRange" name="searchScope" value="all">
+                    <label class="scope-label-item" for="fullRange">全範囲</label>
+                </li>
+            </ul>
+        </div>
     </div>
-    {{-- Suggestion Words --}}
-    <div class="suggestion">
-        <span>検索候補:</span>
-        <ul class="suggestion-list">
-            <li class="suggestion-item">laravel</li>
-            <li class="suggestion-item">composer</li>
-            <li class="suggestion-item">install</li>
-        </ul>
-    </div>
-    {{-- Search Mode --}}
-    <div class="search-mode">
-        <span>検索モード:</span>
-    </div>
+    {{-- Search Body --}}
     <div class="search-body">
-        検索結果:
+        <div class="search-result-label">検索結果:</div>
         <ul class="search-results">
             @for ($i = 0; $i < 30; $i++)
-                <li class="search-result"><a href="">記事タイトル{{ $i }}</a></li>
+                <li class="search-result-item"><a href="">記事タイトル{{ $i }}</a></li>
             @endfor
         </ul>
     </div>
