b94f30c04ec93aac1eb2b6bd56eb26460c68ebc9
rk-techs <ryskdevelop@gmail.com>
2023-09-03 16:00:47 +0900

    refactor: update search.js for readability

--- src/public/js/search.js
+++ src/public/js/search.js
@@ -1,29 +1,35 @@
 'use strict';
 
-/*
----------------------------------------
-| Toggle search modal window
----------------------------------------
-*/
+const htmlBody            = document.body;
+const searchModalTrigger  = document.getElementById('searchModalTrigger');
+const searchModal         = document.getElementById('searchModal');
+const mobileModalClose    = document.getElementById('mobileModalClose');
+
+/**
+ * Toggle search modal window and body class
+ */
 function toggleSearchModal() {
   searchModal.classList.toggle('disable');
   htmlBody.classList.toggle('modal-open');
 }
 
-const htmlBody           = document.body;
-const searchModalTrigger = document.getElementById('searchModalTrigger');
-const searchModal        = document.getElementById('searchModal');
-const mobileModalClose   = document.getElementById('mobileModalClose');
+/**
+ * Check if the event target should close the modal
+ * @param {EventTarget} target - The event target
+ * @returns {boolean} - Whether the modal should be closed
+ */
+function shouldCloseModal(target) {
+    const isModalBackground = target.classList.contains('layout-modal');
+    const isNotModalContent = !target.classList.contains('modal-container');
+    
+    return isModalBackground && isNotModalContent;
+}
 
-searchModalTrigger.addEventListener('click', () => {
-  toggleSearchModal();
-});
-searchModal.addEventListener('click', e => {
-  const target = e.target;
-  if (target.classList.contains('layout-modal') && !target.classList.contains('modal-container')) {
-      toggleSearchModal();
+// Event Listeners
+searchModalTrigger.addEventListener('click', toggleSearchModal);
+searchModal.addEventListener('click', (e) => {
+  if (shouldCloseModal(e.target)) {
+    toggleSearchModal();
   }
 });
-mobileModalClose.addEventListener('click', () => {
-  toggleSearchModal();
-});
+mobileModalClose.addEventListener('click', toggleSearchModal);
