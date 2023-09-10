a4052e5d88082ccb629e2fae14d652447f74f949
rk-techs <ryskdevelop@gmail.com>
2023-09-10 10:57:42 +0900

    refactor: improve filter.js (#29)
    * refactor fetch and render logic for posts list
    
    * Decompose monolithic function into smaller, focused functions for clarity.
    * Improve error handling with fetch operation.
    * Simplify post rendering logic by extracting repeated tasks.
    
    * refactor: decompose performFilter function and add Doc comments
    
    * Separated logic of performFilter into helper functions: getSelectedValues and buildFilterURL.
    * Added detailed Doc comments for each function explaining their purposes, parameters, and return values.
    
    * update methods and add Doc comments
    
    * add Doc comments
    
    * refactor: improve event listener organization and encapsulation
    
    * Moved mobileFilterBlock definition inside its respective function for better scope management.
    * Renamed submitForm function to addFilterOnChange for clarity.
    * Grouped event listener initializations under a single function called initFilterEventListeners.

--- src/public/js/filter.js
+++ src/public/js/filter.js
@@ -1,160 +1,138 @@
 'use strict';
 
 /**
- * 指定したidのセレクトボックが変更された時
+ * Adds a change event listener to the element with the specified ID to trigger the performFilter function.
  *
- * @param {string} targetIdName -セレクトボックスのID
+ * @param {string} targetIdName - The ID of the select box.
  */
-function submitForm(targetIdName) {
-    document.getElementById(targetIdName).addEventListener('change', () => {
-        performFilter();
-    });
+function addFilterOnChange(targetIdName) {
+  const element = document.getElementById(targetIdName);
+  element.addEventListener('change', performFilter);
 }
 
-submitForm('categorySelector');
-submitForm('tagOptionSelector');
-
-/** 複数のタグチェックボックス */
-const checkboxes = document.getElementsByName('tag_slugs[]');
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', () => {
-        const label = checkbox.closest('.tag-label');
-        checkbox.checked ? label.classList.add('tag-checked') : label.classList.remove('tag-checked');
-
-        performFilter();
-    });
-});
-
-const mobileFilterTrigger = document.getElementById('mobileFilterTrigger');
-const mobileFilterBlock = document.getElementById('mobileFilterBlock');
-const mobileFilterBack = document.getElementById('mobileFilterBack');
-
-mobileFilterTrigger.addEventListener('click', () => {
+/**
+* Toggles the visibility of the mobile filter and modifies the body class accordingly.
+*/
+function toggleMobileFilter() {
+  const mobileFilterBlock = document.getElementById('mobileFilterBlock');
   mobileFilterBlock.classList.toggle('show');
-  document.body.classList.toggle('filter-open')
-});
+  document.body.classList.toggle('filter-open');
+}
 
-mobileFilterBack.addEventListener('click', () => {
-  mobileFilterBlock.classList.toggle('show');
-  document.body.classList.toggle('filter-open')
-});
+/**
+* Initialize event listeners for filtering.
+*/
+function initFilterEventListeners() {
+  addFilterOnChange('categorySelector');
+  addFilterOnChange('tagOptionSelector');
+
+  const checkboxes = document.getElementsByName('tag_slugs[]');
+  checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', () => {
+          const label = checkbox.closest('.tag-label');
+          checkbox.checked ? label.classList.add('tag-checked') : label.classList.remove('tag-checked');
+          performFilter();
+      });
+  });
+
+  // Mobile filter toggling
+  const mobileFilterTrigger = document.getElementById('mobileFilterTrigger');
+  const mobileFilterBack = document.getElementById('mobileFilterBack');
+
+  mobileFilterTrigger.addEventListener('click', toggleMobileFilter);
+  mobileFilterBack.addEventListener('click', toggleMobileFilter);
+}
+
+// Initialize the filter event listeners.
+initFilterEventListeners();
 
 /* -----------------------
 Ajax
 ----------------------- */
+/**
+ * Fetches and renders posts based on user's filter criteria.
+ *
+ * 1. Gets the user-selected values.
+ * 2. Constructs the URL with the selected values.
+ * 3. Fetches the posts using the constructed URL.
+ * 4. Renders the posts if any data is returned.
+ */
 function performFilter() {
-  const selectedCategory = document.getElementById("categorySelector").value;
+  const selections = getSelectedValues();
+  const url = buildFilterURL(ajaxFilterRoute, selections);
+
+  fetchPosts(url).then(data => {
+      if (data) renderPosts(data);
+  });
+}
+
+/**
+ * Retrieves the selected filter values from the user interface.
+ *
+ * @returns {Object} - An object containing values selected by the user: category, tag option, and tag slugs.
+ */
+function getSelectedValues() {
+  const selectedCategory  = document.getElementById("categorySelector").value;
   const selectedTagOption = document.getElementById("tagOptionSelector").value;
-  const selectedTagSlugs = Array.from(document.querySelectorAll('input[name="tag_slugs[]"]:checked')).map(e => e.value);
+  const selectedTagSlugs  = Array.from(document.querySelectorAll('input[name="tag_slugs[]"]:checked')).map(e => e.value);
 
-  console.log(ajaxFilterRoute);
+  return {
+      category: selectedCategory,
+      tagOption: selectedTagOption,
+      tagSlugs: selectedTagSlugs
+  };
+}
 
+/**
+ * Constructs a URL based on the given base route and user-selected filter values.
+ *
+ * @param {string} baseRoute - The base endpoint for the filter request.
+ * @param {Object} selections - An object containing the user's filter criteria.
+ * @param {string} selections.category - The selected category slug.
+ * @param {Array<string>} selections.tagSlugs - An array of selected tag slugs.
+ * @param {string} selections.tagOption - The selected tag option value.
+ *
+ * @returns {string} - The fully constructed URL for fetching filtered results.
+ */
+function buildFilterURL(baseRoute, selections) {
   const params = new URLSearchParams();
-  params.append('category_slug', selectedCategory);
-  selectedTagSlugs.forEach(tag => params.append('tag_slugs[]', tag));
-  params.append('tag_option', selectedTagOption);
+  params.append('category_slug', selections.category);
+  selections.tagSlugs.forEach(tag => params.append('tag_slugs[]', tag));
+  params.append('tag_option', selections.tagOption);
 
-  const url = `${ajaxFilterRoute}?${params.toString()}`;
-  console.log(url);
-
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Network response was not ok, status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Received data:', data);
-
-      // posts-listをクリア
-      const postsList = document.querySelector('.posts-list');
-      postsList.innerHTML = '';
-
-      const filterBody = document.querySelector('.filter-body');
-      if (filterBody) {
-          updateOrInsertFilterResult(filterBody, data.filteredPostCount);
-      }
-
-      data.posts.forEach((post, index) => {
-        const postItem = document.createElement('div');
-        postItem.classList.add('post-item');
-
-        const postLink = document.createElement('a');
-        postLink.classList.add('post-item-link');
-        postLink.href = `/posts/${post.slug}`;
-
-        // published_at
-        const postPublishedAt = document.createElement('div');
-        postPublishedAt.classList.add('post-published-at');
-        postPublishedAt.textContent = formatDateToYMD(post.published_at);
-
-        // title
-        const postTitle = document.createElement('h2');
-        postTitle.classList.add('post-title');
-        postTitle.textContent = post.title;
-
-        // tags
-        const tagsList = document.createElement('div');
-        tagsList.classList.add('tags-list');
-
-        if (post.tags) {
-          post.tags.forEach(tag => {
-            const tagItem = document.createElement('div');
-            tagItem.classList.add('tag-item');
-
-            const tagLabel = document.createElement('span');
-            tagLabel.classList.add('tag-label', 'ignore-pointer');
-            tagLabel.textContent = tag.name;
-
-            tagItem.appendChild(tagLabel);
-            tagsList.appendChild(tagItem);
-          });
-        } else {
-          console.warn('Tags are undefined for post', post);
-        }
-
-        // row number
-        const rowNum = document.createElement('div');
-        rowNum.classList.add('post-row-num');
-        rowNum.textContent = data.posts.length - index;
-
-        // Assemble the post item
-        postLink.appendChild(postPublishedAt);
-        postLink.appendChild(postTitle);
-        postLink.appendChild(tagsList);
-        postLink.appendChild(rowNum);
-
-        postItem.appendChild(postLink);
-        postsList.appendChild(postItem);
-      });
-
-    })
-    .catch(error => {
-      insertErrorElement('検索エラーが発生しました... ;(');
-      console.error('There was a problem with the fetch operation:', error);
-    });
-
+  return `${baseRoute}?${params.toString()}`;
 }
 
-function createErrorElement(errorMessage) {
-  const errorElement = document.createElement('div');
-  errorElement.classList.add('filter-invalid-feedback');
-  errorElement.textContent = errorMessage;
-  return errorElement;
+/**
+ * Renders post data on the page by clearing the existing posts list,
+ * updating the filtered posts count, and appending each post to the posts list.
+ *
+ * @param {Object} data - The data containing posts and filtered post count.
+ */
+function renderPosts(data) {
+  console.log('Received data:', data);
+  clearPostsList();
+
+  const filterBody = document.querySelector('.filter-body');
+  if (filterBody) {
+      updateOrInsertFilterResult(data.filteredPostCount);
+  }
+
+  const postsList = document.querySelector('.posts-list');
+  data.posts.forEach((post, index) => {
+      const postElement = createPostElement(post, index, data.posts.length);
+      postsList.appendChild(postElement);
+  });
 }
 
-function insertErrorElement(errorMessage) {
-  const errorElement = createErrorElement(errorMessage);
-
-  const filterBlock = document.getElementById('filterBlock');
-  const filterBody = filterBlock.querySelector('.filter-body');
-
-  filterBody.insertAdjacentElement('beforebegin', errorElement);
-}
-
-function updateOrInsertFilterResult(filterBody, count) {
-  const existingFilterResult = document.querySelector(".filter-result");
+/**
+ * Updates the filter result count on the page, or inserts a new count if it doesn't exist.
+ *
+ * @param {number} count - The number of posts that match the filter.
+ */
+function updateOrInsertFilterResult(count) {
+  const filterBody = document.querySelector('.filter-body');
+  const existingFilterResult = filterBody.querySelector(".filter-result");
 
   if (existingFilterResult) {
       existingFilterResult.textContent = `${count} 件`;
@@ -165,6 +143,12 @@ function updateOrInsertFilterResult(filterBody, count) {
   filterBody.insertAdjacentElement('beforeend', filterResultElement);
 }
 
+/**
+ * Creates a new DOM element to display the filtered post count.
+ *
+ * @param {number} count - The number of posts that match the filter.
+ * @returns {HTMLElement} - The new DOM element.
+ */
 function createFilterResultElement(count) {
   const resultDiv = document.createElement('div');
   resultDiv.classList.add('filter-result');
@@ -172,11 +156,127 @@ function createFilterResultElement(count) {
   return resultDiv;
 }
 
+/**
+ * Formats a date string into YYYY-MM-DD format.
+ *
+ * @param {string} dateString - The input date string.
+ * @returns {string} - The formatted date string.
+ */
 function formatDateToYMD(dateString) {
   const date  = new Date(dateString);
-  const year  = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day   = String(date.getDate()).padStart(2, '0');
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+}
 
-  return `${year}-${month}-${day}`;
+/**
+ * Fetches posts data from the given URL.
+ *
+ * @param {string} url - The URL to fetch posts from.
+ * @returns {Promise<Object>|undefined} - A promise that resolves with the fetched data, or undefined if an error occurred.
+ */
+function fetchPosts(url) {
+  return fetch(url)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`Network response was not ok, status: ${response.status}`);
+          }
+          return response.json();
+      })
+      .catch(error => {
+          insertErrorElement('検索エラーが発生しました... ;(');
+          console.error('There was a problem with the fetch operation:', error);
+      });
+}
+
+/**
+ * Appends a new tag to the provided tags list.
+ *
+ * @param {HTMLElement} tagsList - The element to which the tag should be appended.
+ * @param {string} tagName - The name of the tag.
+ */
+function appendTag(tagsList, tagName) {
+  const tagItem = document.createElement('div');
+  tagItem.classList.add('tag-item');
+
+  const tagLabel = document.createElement('span');
+  tagLabel.classList.add('tag-label', 'ignore-pointer');
+  tagLabel.textContent = tagName;
+
+  tagItem.appendChild(tagLabel);
+  tagsList.appendChild(tagItem);
+}
+
+/**
+ * Creates a new post element using the provided post data.
+ *
+ * @param {Object} post - The post data.
+ * @param {number} index - The index of the post in the list.
+ * @param {number} totalPosts - The total number of posts.
+ * @returns {HTMLElement} - The new post element.
+ */
+function createPostElement(post, index, totalPosts) {
+  const postItem = document.createElement('div');
+  postItem.classList.add('post-item');
+
+  const postLink = document.createElement('a');
+  postLink.classList.add('post-item-link');
+  postLink.href = `/posts/${post.slug}`;
+
+  const postPublishedAt = document.createElement('div');
+  postPublishedAt.classList.add('post-published-at');
+  postPublishedAt.textContent = formatDateToYMD(post.published_at);
+
+  const postTitle = document.createElement('h2');
+  postTitle.classList.add('post-title');
+  postTitle.textContent = post.title;
+
+  const tagsList = document.createElement('div');
+  tagsList.classList.add('tags-list');
+  post.tags && post.tags.forEach(tag => appendTag(tagsList, tag.name));
+
+  const rowNum = document.createElement('div');
+  rowNum.classList.add('post-row-num');
+  rowNum.textContent = totalPosts - index;
+
+  postLink.appendChild(postPublishedAt);
+  postLink.appendChild(postTitle);
+  postLink.appendChild(tagsList);
+  postLink.appendChild(rowNum);
+
+  postItem.appendChild(postLink);
+
+  return postItem;
+}
+
+/**
+ * Clears all posts from the posts list.
+ */
+function clearPostsList() {
+  const postsList = document.querySelector('.posts-list');
+  postsList.innerHTML = '';
+}
+
+/**
+ * Creates a new error element using the provided error message.
+ *
+ * @param {string} errorMessage - The error message.
+ * @returns {HTMLElement} - The new error element.
+ */
+function createErrorElement(errorMessage) {
+  const errorElement = document.createElement('div');
+  errorElement.classList.add('filter-invalid-feedback');
+  errorElement.textContent = errorMessage;
+  return errorElement;
+}
+
+/**
+ * Inserts a new error element before the filter body using the provided error message.
+ *
+ * @param {string} errorMessage - The error message.
+ */
+function insertErrorElement(errorMessage) {
+  const errorElement = createErrorElement(errorMessage);
+  const filterBlock  = document.getElementById('filterBlock');
+  const filterBody   = filterBlock.querySelector('.filter-body');
+
+  filterBody.insertAdjacentElement('beforebegin', errorElement);
 }
