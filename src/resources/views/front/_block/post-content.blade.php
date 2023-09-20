d170e96f4bb7c81f623e6dbc5780fb7827ca23c9
rk-techs <ryskdevelop@gmail.com>
2023-09-20 19:49:46 +0900

    fix(ui): display updated_at on post-content only when it exists

--- src/resources/views/front/_block/post-content.blade.php
+++ src/resources/views/front/_block/post-content.blade.php
@@ -3,7 +3,9 @@
         <h1 class="post-title">{{ $post->title }}</h1>
         <div class="post-datetime">
             <div class="published-date">公開: {{ $post->published_at?->format('Y-m-d') }}</div>
+            @if ($post->updated_at)
             <div class="updated-date">更新: {{ $post->updated_at?->format('Y-m-d') }}</div>
+            @endif
         </div>
     </div>
     <div id="postBody" class="post-body">
