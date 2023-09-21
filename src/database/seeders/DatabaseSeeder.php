08c0993ae9139be5d2266abf0b0182767e435f2a
rk-techs <ryskdevelop@gmail.com>
2023-09-22 07:53:12 +0900

    add migration, model and seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             TagSeeder::class,
             PostSeeder::class,
+            PageSeeder::class,
         ]);
     }
 }
