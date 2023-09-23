c7c1c00b11431dc7dbabfbd33da1f230e63d9cba
rk-techs <ryskdevelop@gmail.com>
2023-09-24 08:42:23 +0900

    feat: add my SNS link

--- src/config/myconf.php
+++ src/config/myconf.php
@@ -2,4 +2,6 @@
 
 return [
     'login_route' => env('LOGIN_ROUTE', 'login'),
+    'my_twitter' => env('MY_TWITTER', 'https://twitter.com/'),
+    'my_github' => env('MY_GITHUB', 'https://github.com/'),
 ];
--- src/resources/views/front/_block/profile.blade.php
+++ src/resources/views/front/_block/profile.blade.php
@@ -9,10 +9,10 @@
             <a href="{{ route('pages.profile') }}" class="nickname">Ryosuke</a>
         </div>
         <div class="sns-icons">
-            <a href="#">
+            <a href="{{ config('myconf.my_twitter') }}" target="_blank" rel="noopener noreferrer">
                 <img src="{{ asset('images/Logo blue.svg') }}" alt="X Logo" class="icon-twitter">
             </a>
-            <a href="#">
+            <a href="{{ config('myconf.my_github') }}" target="_blank" rel="noopener noreferrer">
                 <img src="{{ asset('images/logo-github.svg') }}" alt="GitHub Logo" class="icon-github">
             </a>
         </div>
