d66e579698cc88af44e36388e416f0b73aa4f5f0
rk-techs <ryskdevelop@gmail.com>
2023-09-23 19:36:35 +0900

    feat: add footer nav menu and adjust height

--- src/resources/views/front/_layout/footer.blade.php
+++ src/resources/views/front/_layout/footer.blade.php
@@ -1,5 +1,19 @@
 <footer class="layout-footer">
     <div class="footer-container">
-        <p><small>Copyright © 2023 rk-techs All right reserved.</small></p>
+        <nav class="footer-nav">
+            <ul class="footer-list">
+                <li class="footer-list-item">
+                    <a href="/privacy-policy" class="footer-list-link">Privacy Policy</a>
+                </li>
+                <li class="footer-list-item">
+                    <a href="/about" class="footer-list-link">About</a>
+                </li>
+                <li class="footer-list-item">
+                    <a href="/contact" class="footer-list-link">Contact</a>
+                </li>
+            </ul>
+        </nav>
+
+        <p class="copyright"><small>Copyright © 2023 rk-tecks All rights reserved.</small></p>
     </div>
 </footer>
