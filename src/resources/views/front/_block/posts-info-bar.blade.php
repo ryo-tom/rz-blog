0be1b145e361eb0f1bbf6fbfc971d25dee294d11
rk-techs <ryskdevelop@gmail.com>
2023-09-26 22:16:55 +0900

    fix(ui): replace button with div to fix layouts for iPhone

--- src/resources/views/front/_block/posts-info-bar.blade.php
+++ src/resources/views/front/_block/posts-info-bar.blade.php
@@ -7,13 +7,13 @@
     @endif
 
     <div class="info-for-mobile">
-        <button id="mobileFilterTrigger" class="open-filter-button">
+        <div id="mobileFilterTrigger" class="open-filter-button">
             <span class="material-symbols-outlined">
             filter_list
             </span>
             <div class="label">
                 Filter
             </div>
-        </button>
+        </div>
     </div>
 </div>
