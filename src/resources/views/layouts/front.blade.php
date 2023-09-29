e6067d9099ea260a0bc61ef904d53744ad1f7b93
rk-techs <ryskdevelop@gmail.com>
2023-09-29 20:43:14 +0900

    feat: add canonical tag

--- src/resources/views/layouts/front.blade.php
+++ src/resources/views/layouts/front.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @yield('meta')
+    <link rel="canonical" href="{{ Request::url() }}" />
 
     {{-- Styles --}}
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
